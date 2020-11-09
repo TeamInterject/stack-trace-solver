8ce54a9dbf71abee65dac9103f35e8cf751957f0
Ainoras Zukauskas <ainoraz@gmail.com>
2020-11-09 15:02:25 +0100

    API for stack links

--- src/stack_exchange.py
+++ src/stack_exchange.py
@@ -1,17 +1,17 @@
 import requests
 import json
+import re
 
-
-def get_stackoverflow_links(keyword):
-    BASEURL = "https://api.stackexchange.com/2.2/search"
+def get_stackoverflow_links(keyword, tags = []):
+    BASEURL = "https://api.stackexchange.com/2.2/search/advanced"
 
     params = {
         "page": "1",
         "pagesize": "5",
         "order": "desc",
         "sort": "relevance",
-        "intitle": keyword,
-        "tagged": "java",
+        "q": keyword,
+        "tagged": " ".join(tags),
         "site": "stackoverflow"
     }
 
@@ -20,3 +20,22 @@ def get_stackoverflow_links(keyword):
     temp = json.dumps(r.json(), indent=4, sort_keys=True)
     result = json.loads(temp)
     return result
+
+
+knowladge_base = {
+    "java.lang.NumberFormatException": [r'(For input string)(?:.+)'],
+    "java.sql.SQLException": [r'(Violation of unique constraint )(?:.+ )(duplicate value\(s\) for column\(s\) )(?:.+ )(in statement) (?:.+)'],
+}
+
+def format_stackoverflow_query_string(exception, stack):
+    if exception not in knowladge_base:
+        # TODO: Do more processing. Tokenize, try to get rid of irregular words, etc.
+        return stack
+
+    for fact in knowladge_base[exception]:
+        found = re.search(fact, stack)
+        if bool(found):
+            msg = "".join(found.groups())
+            return [f"{exception}: {msg}", msg]
+    
+    return stack
