2c697dc5009a535348f6035d0f6f65a73a2f0818
Ainoras Zukauskas <ainoraz@gmail.com>
2020-11-21 00:09:36 +0100

    Workarounds to rate limiting and bugs which might not exist

--- src/eclipse_downloader.py
+++ src/eclipse_downloader.py
@@ -2,29 +2,55 @@ import requests
 from datetime import datetime, timedelta
 import time
 import os.path
+import random
+
+
+def generate_file_name(id):
+    return f"./downloads/incident_{id:06d}.xml"
+
+
+def get_missing_file_ids():
+    array_of_missing = []
+    for id in range(1, 569009):
+        file_name = generate_file_name(id)
+        if not os.path.isfile(file_name):
+            array_of_missing.append(id)
+
+    random.shuffle(array_of_missing)
+    return array_of_missing
+
 
 def download_stacks():
-  for id in range(1, 100):
-    file_name = f"./downloads/incident_{id:06d}.xml"
-    if os.path.isfile(file_name):
-      continue
+    ids = get_missing_file_ids()
+    need_retry = False
 
-    expected_time = datetime.now() + timedelta(seconds=1)
-    response = requests.get(f"https://bugs.eclipse.org/bugs/show_bug.cgi?ctype=xml&id={id}")
+    for id in ids:
+        file_name = generate_file_name(id)
 
-    with open(file_name, "w+") as file:
-      file.write(response.content.decode("utf-8"))
+        expected_time = datetime.now() + timedelta(seconds=1)
+        response = requests.get(f"https://bugs.eclipse.org/bugs/show_bug.cgi?ctype=xml&id={id}")
 
-    current_time = datetime.now()
-    if expected_time > current_time:
-      inter = expected_time - current_time
-      time.sleep(inter.microseconds / 1000000)
+        if not response.ok:
+            need_retry = True
+            continue
 
-finished = False
-while finished:
-  try:
-    download_stacks()
-    finished = True
-  except:
-    time.sleep(120)
-    continue
+        with open(file_name, "w+") as file:
+            file.write(response.content.decode("utf-8"))
+
+        current_time = datetime.now()
+        if expected_time > current_time:
+            inter = expected_time - current_time
+            time.sleep(inter.microseconds / 1000000)
+
+    return need_retry
+
+
+need_download = True
+while need_download:
+    try:
+        need_download = download_stacks()
+    except Exception as Ex:
+        print(Ex)
+        continue
+    finally:
+        time.sleep(120)
