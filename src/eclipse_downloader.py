5ce36f9649ec4a71a2c04be30b975a1870a962c8
Ainoras Zukauskas <ainoraz@gmail.com>
2020-11-21 13:20:13 +0100

    Added better splitting

--- src/eclipse_downloader.py
+++ src/eclipse_downloader.py
@@ -5,7 +5,7 @@ import time
 import os.path
 import random
 import _thread
-import argparse
+from tqdm import tqdm
 
 def generate_file_name(id):
     return f"./downloads/incident_{id:06d}.xml"
@@ -22,33 +22,32 @@ def get_missing_file_ids(from_id, to_id):
     return array_of_missing
 
 
-def download_stacks(from_id, to_id):
-    ids = get_missing_file_ids(from_id, to_id)
+def download_stacks(chunk):
     need_retry = False
 
     done = 0
-    for id in ids:
+    for id in chunk:
         file_name = generate_file_name(id)
-        print(f"{done}/{len(ids)}. Downloading report {id} to {file_name}...", end=" ")
+        #print(f"{done}/{len(ids)}. Downloading report {id} to {file_name}...", end=" ")
 
         expected_time = datetime.now() + timedelta(seconds=0.1)
         response = requests.get(f"https://bugs.eclipse.org/bugs/show_bug.cgi?ctype=xml&id={id}")
 
         if not response.ok:
             need_retry = True
-            print("Failed.")
+            #print("Failed.")
             continue
 
         with open(file_name, "w+", encoding="utf-8") as file:
             try:
                 file.write(response.content.decode("utf-8"))
             except:
-                print("Failed.")
+                #print("Failed.")
                 file.close()
                 os.remove(file.name)
                 raise
 
-        print("Done.")
+        #print("Done.")
 
         current_time = datetime.now()
         if expected_time > current_time:
@@ -59,30 +58,31 @@ def download_stacks(from_id, to_id):
 
     return need_retry
 
-def download_with_retry(from_id, to_id):
+def download_with_retry(chunk):
     need_download = True
     while need_download:
         try:
-            need_download = download_stacks(from_id, to_id)
+            need_download = download_stacks(chunk)
         except Exception as Ex:
-            print(Ex)
             continue
         finally:
-            print("Sleeping 5s...")
             time.sleep(5)
 
-splits = 12
-total_start = 0
-total_end = 189669
+def chunks(l, n):
+    n = max(1, n)
+    return (l[i:i+n] for i in range(0, len(l), n))
 
-for i in range(splits):
-    total = total_end - total_start
-    split_amount = total / splits
-    start = int(split_amount * i) + total_start
-    end = int(split_amount * (i+1)) + total_start
+splits = 24
+missing_files = get_missing_file_ids(0, 189669)
+total_left = len(missing_files)
+tbar = tqdm(total=total_left)
+chunked = chunks(missing_files, splits)
 
-    _thread.start_new_thread(download_with_retry, (start, end))
+for chunk in chunked:
+    _thread.start_new_thread(download_with_retry, (chunk,))
 
 while True:
-    sleep(1)
+    current_left = total_left - len(get_missing_file_ids(0, 189669))
+    tbar.update(current_left - tbar.n)
+    sleep(0.5)
     pass
